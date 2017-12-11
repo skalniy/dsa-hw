5ec9d8426b93547a1961bdded0eca6736d3920f1
Skalniy <skalniy@inbox.ru>
2017-12-12 00:23:25 +0300

    main optimized

--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,8 @@
-// #include "stl_wrapper.hxx"
-
 #include <cstdlib>
-
 #include <fstream>
 #include <iostream>
 #include <memory> // unique_ptr
-#include <string>
+#include <string> // to_string
 
 #include HEADER_NAME
 
@@ -14,62 +11,38 @@ main(int argc, char *argv[])
 {
   if (argc < 3)
     return EXIT_FAILURE;
+  
   std::ifstream ifs(argv[1]);
   std::ofstream ofs(argv[2]);
+  ofs.setf(std::ios_base::boolalpha);
 
-  std::unique_ptr<dsa::hash_table> ht_ptr = std::make_unique<dsa::TABLE_NAME>();
-
+  std::unique_ptr<dsa::hash_table> ht_ptr(new dsa::TABLE_NAME());
   while (!ifs.eof() && !ifs.fail())
     {
-      std::string input;
-      ifs >> input;
+      std::string cmd;
+      int key;
+      ifs >> cmd >> key;
 
-      if (input == "add")
-        try
-          {
-            std::string key_str, data_str;
-            ifs >> key_str >> data_str;
-            int key = std::stoi(key_str);
-            int data = std::stoi(data_str);
-            ofs << (ht_ptr->insert(key, data) ? "OK" : "FAIL" ) << std::endl;
-          }
-        catch (std::invalid_argument& e)
-          { 
-            ofs << "error" << std::endl; 
-            return EXIT_FAILURE;
-          }
-      else if (input == "search") 
-        try
-          {
-            std::string key_str;
-            ifs >> key_str;
-            auto result = ht_ptr->search(std::stoi(key_str));
-            if (result)
-              ofs << *result << std::endl;
-            else
-            ofs << "null" << std::endl;
-          }
-        catch (std::invalid_argument& e)
-          { 
-            ofs << "error" << std::endl; 
-            return EXIT_FAILURE;
-          }
-      else if (input == "delete")
-        try
-          {
-            std::string key_str;
-            ifs >> key_str;
-            ofs << (ht_ptr->erase(std::stoi(key_str)) ? "OK" : "FAIL") << std::endl;
-          }
-        catch (std::invalid_argument& e)
-          { 
-            ofs << "error" << std::endl; 
-            return EXIT_FAILURE;
-          }
-      else if (!input.empty())
-        { 
-          ofs << "error" << std::endl; 
-          return EXIT_FAILURE;
+      if (cmd == "add")
+        {
+          int data;
+          ifs >> data;
+
+          auto result = ht_ptr->insert(key, data);
+
+          ofs << result << std::endl;
+        }
+      else if (cmd == "search") 
+        {
+          auto result = ht_ptr->search(key);
+
+          ofs << (result ? std::to_string(*result) : "null") << std::endl;
+        }
+      else if (cmd == "delete")
+        {
+          auto result = ht_ptr->erase(key);
+
+          ofs << result << std::endl;
         }
     }
 
